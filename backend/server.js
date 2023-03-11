80047687d0af9ab6ff9b63d057fca75d2b3cb4f5
solomon james <ejioforjames12@gmail.com>
2023-03-11 00:34:03 -0800

    corek

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,11 @@ app.use('/api/orders', orderRouter)
 
 const __dirname = path.resolve()
 
-app.use(express.static(path.join(__dirname, "/frontend/build")))
+app.use(express.static(path.join(__dirname, "../frontend/build")))
 
 app.get("*", function(req, res) {
   res.sendFile(
-    path.join(__dirname, "/frontend/build/index.html"),
+    path.join(__dirname, "../frontend/build/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err)
