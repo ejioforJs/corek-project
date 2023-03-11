d9c612c7f626b71e182b159828bbea845bfe3291
solomon james <ejioforjames12@gmail.com>
2023-03-11 01:20:48 -0800

    corek

--- backend/server.js
+++ backend/server.js
@@ -32,16 +32,11 @@ const __dirname = path.resolve()
 
 app.use(express.static(path.join(__dirname, "/frontend/build")))
 
-app.get("*", function(req, res) {
+app.get("*", (req, res) =>
   res.sendFile(
-    path.join(__dirname, "/frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    }
+    path.join(__dirname, "/frontend/build/index.html")
   )
-})
+)
 
 app.use((err,req,res,next) => {
     res.status(500).send({message:err.message})
